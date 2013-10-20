7c94aa9a529240ef5e1d92e944a14201b1f11e03
Wyatt Neal <wyatt.neal+git@gmail.com>
2013-10-20 18:02:45 -0400

    Updating to remove nodes

--- src/edu/uc/cs/distsys/ilead/ElectionNotifierThread.java
+++ src/edu/uc/cs/distsys/ilead/ElectionNotifierThread.java
@@ -2,27 +2,20 @@ package edu.uc.cs.distsys.ilead;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Random;
-
-import edu.uc.cs.distsys.Logger;
-import edu.uc.cs.distsys.Node;
 import edu.uc.cs.distsys.comms.CommsWrapper;
 import edu.uc.cs.distsys.comms.MulticastWrapper;
 
 public class ElectionNotifierThread implements Runnable {
 
-	private HashMap<Integer, Node> nodes;
 	private int id;
 	private Random randomNumberGen;
 	private int transactionId;
 	private CommsWrapper<ElectionMessage> electionSender;
 	private static final int ELECTION_PORT = 5100;
 	
-	public ElectionNotifierThread(int currentNodeId, HashMap<Integer, Node> nodes) throws UnknownHostException {
+	public ElectionNotifierThread(int currentNodeId) throws UnknownHostException {
 		this.id = currentNodeId;
-		this.nodes = nodes;
 		this.randomNumberGen = new Random(System.currentTimeMillis());
 		this.electionSender = new MulticastWrapper<ElectionMessage>(ELECTION_PORT, this.id, new ElectionMessage.ElectionFactory(), null);
 	}
