02ffe091531df3968b9773a1f15b184cf3b4a15e
Wyatt Neal <wyatt.neal+git@gmail.com>
2013-10-20 18:00:26 -0400

    probably does thread like things…maybe.

--- src/edu/uc/cs/distsys/ilead/ElectionNotifierThread.java
+++ src/edu/uc/cs/distsys/ilead/ElectionNotifierThread.java
@@ -1,5 +1,7 @@
 package edu.uc.cs.distsys.ilead;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Random;
@@ -18,7 +20,7 @@ public class ElectionNotifierThread implements Runnable {
 	private CommsWrapper<ElectionMessage> electionSender;
 	private static final int ELECTION_PORT = 5100;
 	
-	public ElectionNotifierThread(int currentNodeId, HashMap<Integer, Node> nodes) {
+	public ElectionNotifierThread(int currentNodeId, HashMap<Integer, Node> nodes) throws UnknownHostException {
 		this.id = currentNodeId;
 		this.nodes = nodes;
 		this.randomNumberGen = new Random(System.currentTimeMillis());
@@ -28,19 +30,13 @@ public class ElectionNotifierThread implements Runnable {
 	@Override
 	public void run() {
 		// P broadcasts an election message (inquiry) to all other processes with higher process IDs, expecting an "I am alive" response from them if they are alive.
-		Iterator<Node> nodeIterator = this.nodes.values().iterator();
 		this.transactionId = this.randomNumberGen.nextInt();
 		ElectionMessage e = new ElectionMessage(this.id, this.transactionId);
-		while (nodeIterator.hasNext()) {
-			if (((Node) nodeIterator).getId() > this.id) {
-				// send the message to everyone
-				
-			}
+		try {
+			this.electionSender.send(e);
+		} catch (IOException ioException) {
+			// TODO: yea right, like I care
 		}
-		// If P hears from no process with a higher process ID than it, it wins the election and broadcasts victory.
-		// If P hears from a process with a higher ID, P waits a certain amount of time for that process to broadcast itself as the leader. If it does not receive this message in time, it re-broadcasts the election message.
-		// If P gets an election message (inquiry) from another process with a lower ID it sends an "I am alive" message back and starts new elections.
-
 		
 	}
 	
