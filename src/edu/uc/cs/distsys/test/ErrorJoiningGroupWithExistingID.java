3a461ae0ba71572c65e31dea48618d7f58df1c8f
longca <longca@users.noreply.github.com>
2013-11-16 17:21:21 -0500

    I believe this test for A8 should be correct...
    Unfortunately we are never getting the "Failed to join group" message so
    I think we need code modifications

--- src/edu/uc/cs/distsys/test/ErrorJoiningGroupWithExistingID.java
+++ src/edu/uc/cs/distsys/test/ErrorJoiningGroupWithExistingID.java
@@ -14,7 +14,7 @@ import edu.uc.cs.distsys.init.GroupJoinException;
 public class ErrorJoiningGroupWithExistingID {
 
 	final static int numNodes = 2;
-	final static String PASSED_MSG = "Req A8 Passed: A process must report an error if it attempts to join a group that already has a correct process sharing the same identifier)";
+	final static String PASSED_MSG = "Req A8 Passed: A process must report an error if it attempts to join a group that already has a correct process sharing the same identifier";
 	final static String FAILED_MSG = "Req A8 Failed";
 	private DetectMain orig;
 	private DetectMain impostor;
@@ -34,21 +34,24 @@ public class ErrorJoiningGroupWithExistingID {
 	
 	@Test
 	public void test() throws InterruptedException {
+		String errorMessage = "";
 		// Allow system to stabilize
 		Thread.sleep(3000);
 		
-		// Start the imposter
+		// Start the impostor
+		System.err.println("Impostor is attempting to authenticate as user 1000!");
 		try {
 			impostor.start();
 		} catch (UnknownHostException e) {
 			assertTrue(e.toString(), false);
 		} catch (GroupJoinException e) {
-			assertTrue(e.toString(), false);
+			System.err.println("Impostor is unable to join the group! " + e.getMessage());
+			errorMessage = e.getMessage();
 		}
 		
-		// TODO: assert that an error occurs when impostor starts
-		String message = "";
-		assertTrue(FAILED_MSG, message.equals("Unable to join network"));
+		Thread.sleep(3000);
+		
+		assertTrue(FAILED_MSG, errorMessage.startsWith("Failed to join group") && errorMessage.endsWith(" (node conflict detected)"));
 				
 		System.out.println(PASSED_MSG);
 	}
