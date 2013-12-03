1d68940e0f1359022244b4a9ce0477e4585192f4
matt <mwsheppard@gmail.com>
2013-12-02 23:13:40 -0500

    Cleaned up logger to stop spewing useless messages during shutdown.
    Also added setOutputStreams() method to allow changing logging behavior
    dynamically

--- src/edu/uc/cs/distsys/Logger.java
+++ src/edu/uc/cs/distsys/Logger.java
@@ -1,9 +1,12 @@
 package edu.uc.cs.distsys;
 
+import java.io.PrintStream;
+
 public interface Logger {
 
 	public void log(String msg);
 	public void error(String msg);
 	public void debug(String msg);
 	
+	public void setOutputStreams(PrintStream out, PrintStream err, PrintStream dbg);
 }
